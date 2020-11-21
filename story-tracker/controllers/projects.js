c83500c416c6b1570112120144c434868bb04238
Matt <matt-paul@live.com>
2020-11-20 23:52:31 -0500

    fixed the reference function between the two models!!!!!!!!

--- story-tracker/controllers/projects.js
+++ story-tracker/controllers/projects.js
@@ -10,15 +10,15 @@ function index(req, res) {
     });
 }
 
-function create(req, res) {
-    const project = new Project(req.body);
-    project.user = req.user._id;
-    project.save(function(err) {
-        if (err) return res.redirect('/projects/index');
-        console.log(project)
-        res.redirect('/projects');
-    });
-}
+// function create(req, res) {
+//     const project = new Project(req.body);
+//     project.user = req.user._id;
+//     project.save(function(err) {
+//         if (err) return res.redirect('/projects/index');
+//         console.log(project)
+//         res.redirect('/projects');
+//     });
+// }
 
 // function create(req, res) {
 //     User.findById(req.user._id, function(err, user) {
@@ -32,9 +32,22 @@ function create(req, res) {
 //         })
 //     }
 
+function create(req, res) {
+    User.findById(req.user._id).populate('projects')
+    .exec(function(err, user) {
+        const project = new Project(req.body);
+        user.projects.push(project)
+        user.save()
+        console.log(project)
+        console.log(req.user.projects)
+    })
+    res.redirect('/projects');
+    }
+
 function show(req, res) {
     Project.findById(req.params.id, function(err, project) {
-        User.find({}, function(err, users) {
+        User.findById(req.user._id, function(err, users) {
+            console.log(users)
             res.render('projects/show', {
                 project,
                 users
